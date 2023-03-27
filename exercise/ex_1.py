04e11eea3139821caf63f886c5c459378e80f690
Martyna Kolenda <89539@student.ue.poznan.pl>
2023-03-27 20:18:38 +0200

    Added first task

--- exercise/ex_1.py
+++ exercise/ex_1.py
@@ -1,1 +1,12 @@
-cokolweik
+def hello(a: str) -> str:
+    # TODO
+    return "Hello"
+
+
+print("Example")
+print(hello("Ola"))
+
+assert hello("Ola") == "Hello Ola"
+assert hello("Zuzia") == "Hello Zuzia"
+
+print("Zadanie zrobione")
