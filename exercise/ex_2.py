05789836fde06d6927e3419a34e1fd873d4016e5
Martyna Kolenda <89539@student.ue.poznan.pl>
2023-03-27 12:33:14 +0200

    prostokąt

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -6,4 +6,12 @@ pole = a * a
 
 print("Obwód kwadratu wynosi: " + str(obwod) + ", a pole: " + str(pole) + ".")
 
+# prostokąt
+a = 10
+b = 5
+
+obwod = 2 * a + 2 * b
+pole = a * b
+print("Obwód prostokąta wynoi: " + str(obwod) + ", a pole:" + str(pole) + ".")
+
 
